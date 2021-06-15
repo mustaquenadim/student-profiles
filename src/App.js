56c46e0a63af3d0fdb5f3548ac842b1fba1cf524
mustaquenadim <mustaquenadim2000@gmail.com>
2021-06-15 12:32:50 +0600

    Almost done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Profile from './components/Profile/Profile'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [students, setStudents] = useState([]);
+    const [tags, setTags] = useState([]);
+    const [search, setSearch] = useState([]);
+    useEffect(() => {
+        fetch('https://api.hatchways.io/assessment/students')
+        .then(response => response.json())
+        .then(data => {
+            setStudents(data.students);
+            console.log(students);
+        });
+    }, [])
+    const handleAddTag = (event, id) => {
+        if (event.key === "Enter" && event.target.value !== "") {
+            setTags([...tags, event.target.value]);
+            event.target.value = "";
+            const stags = students.find(item => item.id === id);
+            console.log(stags);
+            setStudents([...students, tags]);
+        }
+    }
+    
+    console.log(students);
+    // console.log(tags);
+    const handleNameSearch = (event) => {
+        setSearch(event.target.value);
+    }
+    const handleTagSearch = (event) => {
+
+    }
+    return (
+        <div className="parent">
+            <div className="child border" style={{borderRadius: "10px"}}>
+                <div className="search">
+                    <input type="search" placeholder='Search By name' onChange={handleNameSearch} />
+                    <input type="search" placeholder='Search By tag' onChange={handleTagSearch} />
+                </div>
+                {students.filter((value) => {
+                    if (search == '') {
+                        return value;
+                    } else if (value.firstName.toLowerCase().includes(search.toLowerCase())||value.lastName.toLowerCase().includes(search.toLowerCase())){
+                        return value;
+                    }
+                }).map((data) => (
+                    <Profile key={data.id} data={data} handleAddTag={handleAddTag}></Profile>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { FaPlus, FaMinus } from 'react-icons/fa';
+
+const Profile = ({data, handleAddTag}) => {
+    const [isHidden, setIsHidden] = useState(true);
+    const {id, pic, firstName, lastName, email, company, skill, grades, tags} = data;
+    let sum = 0;
+    grades?.map(grade => sum += Number(grade));
+    const average = sum / grades?.length;
+    const toggle = () => {
+        setIsHidden(!isHidden);
+    }
+    // const [tags, setTags] = useState([]);
+    // const handleAddTag = (event, id) => {
+    //     if (event.key === "Enter" && event.target.value !== "") {
+    //         setTags([...tags, event.target.value]);
+    //         event.target.value = "";
+    //         const stags = students.find(item => item.id === id);
+    //         console.log(stags);
+    //         setStudents([...students, tags]);
+    //     }
+    // }
+    return (
+        <div className="container border-bottom">
+            <div className="row">
+                <div className="col-lg-3 d-flex justify-content-center align-items-center">
+                    <img className="border rounded-circle w-75" src={pic} alt={lastName} />
+                </div>
+                <div className="col-lg-8">
+                    <h1>{firstName} {lastName}</h1>
+                    <div className="container lh-1">
+                        <p>Email: {email}</p>
+                        <p>Company: {company}</p>
+                        <p>Skill: {skill}</p>
+                        <p>Average: {average}%</p>
+                        <dl className="d-flex">
+                        {tags?.map((tag, index) => (
+                            <dd key={index} className="tag">{tag}</dd>
+                        ))}
+                        </dl>
+                        <input type="text" placeholder="Add a tag" id={id} onKeyUp={(e) => handleAddTag(e, id)} />
+                    </div>
+                </div>
+                <div className="col-lg-1 my-auto">
+                    <button className="btn" onClick={toggle}>
+                        <span>{isHidden ? <FaPlus className="icon" /> : <FaMinus className="icon" />}</span>
+                    </button>
+                </div>
+            </div>
+            <div>
+            {!isHidden && (
+                <ul className="list-group lh-1">
+                    {grades.map((test, index) => (<li key={index} className="list-group-item border-0">Test {index + 1}: {test}%</li>))}
+                </ul>
+            )}
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
