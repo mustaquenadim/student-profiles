4ab19297e56aa2a888e305b9190d7a105e304fc4
mustaquenadim <mustaquenadim2000@gmail.com>
2021-07-05 15:21:19 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Profile from './components/Profile/Profile'
+import Profile from './components/Profile/Profile';
 
 const App = () => {
     const [students, setStudents] = useState([]);
@@ -8,52 +8,53 @@ const App = () => {
     const [tagSearch, setTagSearch] = useState([]);
     useEffect(() => {
         fetch('https://api.hatchways.io/assessment/students')
-        .then(response => response.json())
-        .then(data => {
-            setStudents(data.students);
-        });
-    }, [])
-    
+            .then((response) => response.json())
+            .then((data) => {
+                setStudents(data.students);
+            });
+    }, []);
+
     const handleAddTag = (event, id) => {
-        if (event.key === "Enter" && event.target.value !== "") {
-            setStudents(students.map((row, i) => {
-                if (row.id === id) {
-                    let newTag = row.tags ? [...row.tags] : [];
-                    return {
-                        ...row, tags: [...newTag, event.target.value]
-                    };
-                } else {
-                    return row;
-                }
-            }));
-            event.target.value = "";
+        if (event.key === 'Enter' && event.target.value !== '') {
+            setStudents(
+                students.map((row, i) => {
+                    if (row.id === id) {
+                        let newTag = row.tags ? [...row.tags] : [];
+                        return {
+                            ...row,
+                            tags: [...newTag, event.target.value],
+                        };
+                    } else {
+                        return row;
+                    }
+                })
+            );
+            event.target.value = '';
         }
     };
-    const handleNameSearch = (event) => {
-        setNameSearch(event.target.value);
-    }
-    const handleTagSearch = (event) => {
-        setTagSearch(event.target.value);
-    }
-    console.log(students);
+
     return (
-        <div className="parent">
-            <div className="child border" style={{borderRadius: "10px"}}>
-                <div className="search">
-                    <input type="search" placeholder='Search By name' onChange={handleNameSearch} />
-                    <input type="search" placeholder='Search By tag' onChange={handleTagSearch} />
+        <div className='parent'>
+            <div className='child border' style={{ borderRadius: '10px' }}>
+                <div className='search'>
+                    <input type='search' placeholder='Search By name' onChange={(e) => setNameSearch(e.target.value.toLowerCase())} />
+                    <input type='search' placeholder='Search By tag' onChange={(e) => setTagSearch(e.target.value.toLowerCase())} />
                 </div>
-                {students.filter((value) => {
-                    if (nameSearch == '') {
-                        console.log(value);
-                        return value;
-                    } else if (value.firstName.toLowerCase().includes(nameSearch.toLowerCase()) || value.lastName.toLowerCase().includes(nameSearch.toLowerCase())) {
-                        console.log(value);
-                        return value;
-                    }
-                }).map((data) => (
-                    <Profile key={data.id} data={data} handleAddTag={handleAddTag}></Profile>
-                ))}
+                {students
+                    .filter((value) => {
+                        if (tagSearch != '') {
+                            return value?.tags?.includes(tagSearch);
+                        } else if (nameSearch == '' && tagSearch == '') {
+                            return value;
+                        } else if (nameSearch != '') {
+                            if (value.firstName.toLowerCase().includes(nameSearch.toLowerCase()) || value.lastName.toLowerCase().includes(nameSearch.toLowerCase())) {
+                                return value;
+                            }
+                        }
+                    })
+                    .map((data) => (
+                        <Profile key={data.id} data={data} handleAddTag={handleAddTag}></Profile>
+                    ))}
             </div>
         </div>
     );
